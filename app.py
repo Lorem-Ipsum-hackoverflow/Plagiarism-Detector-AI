b34e37d158b0f752d02d012f481bdefb97f6b1a3
Sinchana-SH <116704673+Sinchana-SH@users.noreply.github.com>
2023-03-17 08:37:12 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,50 @@
+from flask import Flask,render_template,request
+import re
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+
+app = Flask(__name__)
+
+@app.route('/', methods= ['GET','POST'])
+def index(similarpar = 0):
+        del paras1 , paras2 , par1 , par2, similarpar
+        try :
+            par1 =  request.form.get('para1')
+            par2 = request.form.get('para2')
+            
+            par1 = str(par1)
+            par2 = str(par2)
+            
+            paras1 = preprocess(par1)
+            paras2 = preprocess(par2)
+            vectorizer = TfidfVectorizer().fit_transform([paras2,paras1])
+            print(vectorizer)
+        except ValueError:
+            return render_template('index.html', similarpar = similarpar, par1=par1 , par2=par2)
+        else : 
+            
+            
+            similarity_score = cosine_similarity(vectorizer[0], vectorizer[1])[0][0]
+            print(similarity_score)
+            
+            similarpar = int( similarity_score * 100 )
+            vectorizer = []
+            
+            print(similarpar)
+            
+            
+        return render_template('index.html', similarpar = similarpar, par1=par1 , par2=par2) 
+        
+def preprocess(txt):
+    txt = re.sub('\n', ' ', txt)
+    txt = re.sub('[^\w\s]', '', txt)
+    # Convert to lowercase
+    txt = txt.lower()
+    print(txt)
+    return txt
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
